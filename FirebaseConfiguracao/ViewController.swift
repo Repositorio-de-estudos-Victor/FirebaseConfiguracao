b34b3db712037fa63ebf067902da56a80efb9573
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-05-12 18:51:46 -0300

    Initial Commit

---
+++ FirebaseConfiguracao/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FirebaseConfiguracao
+//
+//  Created by Victor Rodrigues Novais on 12/05/20.
+//  Copyright © 2020 Victoriano. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
