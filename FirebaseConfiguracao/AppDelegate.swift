8b9c8da532038bff237d43c8bf8b9584c7913ba1
Victor Rodrigues Novais <victor96novais@gmail.com>
2020-05-13 20:53:07 -0300

    Configuração do Firebase, poc  Firebase Auth e crud Firebase database realtime

--- FirebaseConfiguracao/AppDelegate.swift
+++ FirebaseConfiguracao/AppDelegate.swift
@@ -7,14 +7,16 @@
 //
 
 import UIKit
+import Firebase
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
-
+    
+    override init() {
+        FirebaseApp.configure()
+    }
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
         return true
     }
 
--- FirebaseConfiguracao/ViewController.swift
+++ FirebaseConfiguracao/ViewController.swift
@@ -7,12 +7,75 @@
 //
 
 import UIKit
+import Firebase
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var textoLabel: UILabel!
+    
+    let firebase = Database.database().reference()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+//        do {
+//            try Auth.auth().signOut()
+//        } catch {
+//            print("Erro ao deslogar o usuário")
+//        }
+        
+        Auth.auth().signIn(withEmail: "victor@gmail.com", password: "vic123456") { (User, error) in
+            if error == nil {
+                print("Sucesso ao logar o usuário")
+            } else {
+                print("Erro ao logar usuário: \(String(describing: error?.localizedDescription))")
+            }
+        }
+        
+        Auth.auth().addStateDidChangeListener { (Auth, User) in
+            if let usuarioLogado = User {
+                print("Usuario está logado \(String(usuarioLogado.email!))")
+            } else {
+                print("Usuario não está logado")
+            }
+         }
+        
+
+        // Criar usuario no Firebase Auth
+//        Auth.auth().createUser(withEmail: "victor@gmail.com", password: "vic123456") { (user, error) in
+//
+//            if error == nil { //sucesso
+//                print("Sucesso ao cadastrar usuario!")
+//            } else { // erro
+//                print("Erro ao cadastrar usuario: \(String(describing: error?.localizedDescription))")
+//            }
+//
+//        }
+
+//        Auth.auth().addStateDidChangeListener { (Auth, User) in
+//            if let usuarioLogado = User {
+//                print("Usuario está logado \(String(usuarioLogado.email!))")
+//            } else {
+//                print("Usuario não está logado")
+//            }
+//        }
+        
+        
+//
+//        // Adiciona no Firebase Databae Realtime
+//
+//        let pontuacao = firebase.child("pontuacao").child("valor")
+////        pontuacao.child("valor").setValue("200")
+//
+//        // Remove valores do Firebase Database Realtime
+//        //pontuacao.removeValue()
+//
+//        // Recupera os dados do Firebase Database Realtime
+//        pontuacao.observe(DataEventType.value, with: {(dados) in
+//            let ponto = dados.value as? String
+//            self.textoLabel.text = ponto
+//
+//        })
     }
 
 
